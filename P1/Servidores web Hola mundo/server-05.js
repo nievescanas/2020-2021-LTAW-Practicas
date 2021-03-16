308ea9c022f679beb71cd60eadf9a66d06d37cb2
nievescanas <nievescanasmartin@gmail.com>
2021-03-16 13:19:08 +0100

    Servidor 5: Happy server: Añadiendo cabeceras

---
+++ P1/Servidores web Hola mundo/server-05.js
@@ -0,0 +1,32 @@
+// text/plain: Texto plano. Valor por defecto
+// text/html: Texto HTML
+// text/css: Hoja de estilo
+// image/jpeg: Imagen en formato jpeg
+// image/png: Imagen en formato png
+
+tp = require('http');
+
+//-- Definir el puerto a utilizar
+const PUERTO = 8080;
+
+//-- Crear el servidor
+const server = http.createServer((req, res) => {
+    
+  //-- Indicamos que se ha recibido una petición
+  console.log("Petición recibida!");
+
+  //-- Cabecera que indica el tipo de datos del
+  //-- cuerpo de la respuesta: Texto plano
+  res.setHeader('Content-Type', 'text/plain');
+
+  //-- Mensaje del cuerpo
+  res.write("Soy el Happy server!!\n");
+
+  //-- Terminar la respuesta y enviarla
+  res.end();
+});
+
+//-- Activar el servidor: ¡Que empiece la fiesta!
+server.listen(PUERTO);
+
+console.log("Happy server activado!. Escuchando en puerto: " + PUERTO);
