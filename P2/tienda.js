151046faa00951484f8c238cefde3ac235b49d0f
nievescanas <nievescanasmartin@gmail.com>
2021-05-16 17:02:36 +0200

    Copia server P1

---
+++ P2/tienda.js
@@ -0,0 +1,71 @@
+// - Módulo http
+const http = require('http');
+var url = require('url');
+
+// - Puerto donde recibir las peticiones
+const PUERTO = 9000;
+
+//-- Acceso al módulo fs, para lectura de ficheros
+var fs = require('fs');
+var filename
+
+const server = http.createServer((req, res)=>{
+    console.log("Petición recibida!");
+
+    //-- Valores de la respuesta por defecto
+    let code = 200;
+    let code_msg = "OK";
+
+    //-- Tipos mime por petición
+    let mime = "text/"
+    let mime_img = "image/"
+
+
+    //-- Analizar el recurso
+    //-- Construir el objeto url con la url de la solicitud
+    const url = new URL(req.url, 'http://' + req.headers['host']);
+    console.log(url.pathname);
+
+    var filename = "." + url.pathname;
+
+    switch (url.pathname) {
+        case "/":           
+            //-- Generar la respusta en función de las variables
+            //-- code, code_msg y page
+            content = fs.readFileSync("./Home.html", "utf-8")
+            mime = mime + "html"
+            res.statusCode = code;
+            res.statusMessage = code_msg;
+            res.setHeader('Content-Type',mime);
+            res.write(content);
+            res.end();
+            break;
+
+        default:
+            let point_position = url.pathname.lastIndexOf(".")
+            let tipo = url.pathname.slice(point_position+1)
+            if (tipo == "png" || tipo == "jpeg" ) {
+              mime = mime_img + tipo
+            }else{
+              mime = mime + tipo
+            }
+
+            fs.readFile(filename, function(err, data) {
+                if (err) {
+                  content = fs.readFileSync("./Pagina-Error.html", "utf-8")
+                  res.writeHead(404, {'Content-Type': 'text/html'});
+                  res.write(content);
+                  return res.end();
+                }
+                //-- Generar el mensaje de respuesta
+                res.writeHead(200, {'Content-Type': mime});
+                res.write(data);
+                res.end();
+              });
+        break
+   }
+
+});
+
+server.listen(PUERTO);
+console.log("TIENDA. Escuchando en puerto: " + PUERTO);
